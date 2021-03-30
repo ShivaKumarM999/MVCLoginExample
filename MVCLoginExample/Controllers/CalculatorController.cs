30f6045be79eb99f314e357cf6bb850555b213cc
ShivaKumarM999 <81612983+ShivaKumarM999@users.noreply.github.com>
2021-03-30 17:08:25 +0530

    Calculator App

---
+++ MVCLoginExample/Controllers/CalculatorController.cs
@@ -0,0 +1,62 @@
+﻿using MVCLoginExample.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCLoginExample.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        // GET: Calculator
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        //[HttpPost]
+        //public ActionResult Add(Calculator cal)
+        //{
+        //    cal.Result = cal.Num1 + cal.Num2;
+        //    return View(cal);
+        //}
+
+        //[HttpPost]
+        //public ActionResult Add(Calculator cal, string Add, string Sub, string Mul, string Div)
+        //{
+        //    if(!string.IsNullOrEmpty(Add))
+        //    cal.Result = cal.Num1 + cal.Num2;
+        //    if (!string.IsNullOrEmpty(Sub))
+        //        cal.Result = cal.Num1 - cal.Num2;
+        //    if (!string.IsNullOrEmpty(Mul))
+        //        cal.Result = cal.Num1 * cal.Num2;
+        //    if (!string.IsNullOrEmpty(Div))
+        //        cal.Result = cal.Num1 / cal.Num2;
+        //    return View(cal);
+        //}
+
+        //[HttpPost]
+        //public ActionResult Add(Calculator cal, string calsi)
+        //{
+
+        //    switch (calsi)
+        //    {
+        //        case "Add":
+        //            cal.Result = cal.Num1 + cal.Num2;break;
+        //        case "Sub":
+        //            cal.Result = cal.Num1 - cal.Num2; break;
+        //        case "Mul":
+        //            cal.Result = cal.Num1 * cal.Num2; break;
+        //        case "Div":
+        //            cal.Result = cal.Num1 / cal.Num2; break;
+        //    }
+        //    return View(cal);
+        //}
+    }
+}
---
+++ MVCLoginExample/Models/Calculator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCLoginExample.Models
+{
+    public class Calculator
+    {
+        [Required]
+        [Display(Name ="Enter Number1")]
+        public int Num1 { get; set; }
+
+
+        [Required]
+        [Display(Name = "Enter Number2")]
+        public int Num2 { get; set; }
+
+
+        public int Result { get; set; }
+    }
+}
