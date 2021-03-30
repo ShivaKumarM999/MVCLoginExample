c2a4d097f0928f749adbd1b809a0f07dd5c4a1b8
ShivaKumarM999 <81612983+ShivaKumarM999@users.noreply.github.com>
2021-03-30 16:09:08 +0530

    Add project files.

---
+++ MVCLoginExample/Controllers/LoginController.cs
@@ -0,0 +1,38 @@
+﻿using MVCLoginExample.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCLoginExample.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+
+            return View();
+        }
+
+
+
+        [HttpPost]
+        public ActionResult Login(EmpLogin model)
+        {
+            if (model.UserName.Equals("Shiva") && model.Password.Equals("Rama"))
+                return RedirectToAction("Index");
+            else
+            {
+                ViewBag.ErrorMsg = "Invalid UserName & Password";
+                return View();
+            }
+        }
+    }
+}
---
+++ MVCLoginExample/Models/EmpLogin.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCLoginExample.Models
+{
+    public class EmpLogin
+    {
+        [Required]
+        [Display(Name ="Enter User name")]
+        public string UserName { get; set; }
+
+        [Required]
+        [Display(Name = "Enter User name")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
